a94b250915573ed1b9a1c206eccf8658839d43fd
Kurukshetra <kurukshetracodingschool@gmail.com>
2025-01-13 20:51:05 +0530

    Complete Bubble Game

--- script.js
+++ script.js
@@ -1,56 +1,50 @@
 var bottom = document.querySelector(".bottom");
-var hit =  document.querySelector(".hit");
-var incressScore =  document.querySelector(".score");
-var timerdiv =  document.querySelector(".timer")
+var hit = document.querySelector(".hit");
+var increaseScore = document.querySelector(".score");
+var timerDiv = document.querySelector(".timer");
 var count = 30;
-var score  = 0;
+var score = 0;
 var random;
 
-function getbubble () {
-    bottom.innerHTML = ``
-        for (let i  = 0; i<140; i++) {
-            var rn = Math.floor(Math.random()*10);
-        bottom.innerHTML += `<div id = 'bubblediv' >${rn}</div>`
+function getBubble() {
+    bottom.innerHTML = '';
+    for (let i = 0; i < 140; i++) {
+        const rn = Math.floor(Math.random() * 10);
+        bottom.innerHTML += `<div id='bubblediv' data-value>${rn}</div>`;
+    }
+}
+
+function updateScore() {
+    score += 10;
+    increaseScore.textContent = `Score: ${score}`;
+}
+
+function getHit() {
+    random = Math.floor(Math.random() * 10);
+    hit.textContent = random.toString();
+}
+
+bottom.addEventListener('click', function(event) {
+    if (Number(event.target.textContent) === random) {
+        updateScore();
+        getHit();
+        getBubble();
+    } else {
+        bottom.innerHTML = '<h1> Game Over </h1>';
+    }
+});
+
+function startTimer() {
+    setInterval(function() {
+        if (count > 0) {
+            count--;
+            timerDiv.textContent = count.toString();
+        } else {
+            bottom.innerHTML = '<h1> Game Over </h1>';
         }
-    }
-
-function Score (){
-    score +=10
-    incressScore.textContent = `Score: ${score}`
-    
- }   
-
-function gethit(){
-    random = `${Math.floor(Math.random()*10)}`
-    hit.textContent += random;
+    }, 1000);
 }
 
-
-
-bottom.addEventListener('click',function(dets){
-     if(Number(dets.target.textContent) === random){
-    Score();
-    gethit();
-    getbubble();
-    console.log(dets)
-}
-else{
-    bottom.innerHTML = `<h1> Game Over </h1>`
-}
-})
-
-
-function timer(){
-setInterval(function(){
-    if(count>0){
-        count--;
-        timerdiv.textContent = count;
-    }
-    else{
-        bottom.innerHTML =`<h1> Game Over </h1>`;
-    }
-},1000)
-}
-getbubble()
-timer()
-gethit()
+getBubble();
+startTimer();
+getHit();
